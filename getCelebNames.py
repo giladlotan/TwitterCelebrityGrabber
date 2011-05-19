a6ca5bc57d3c9a9313ece19bc48c90a60bbd831d
giladlotan <giladlotan@gmail.com>
2011-05-19 03:14:28 -0400

    adding file save

--- getCelebNames.py
+++ getCelebNames.py
@@ -6,7 +6,11 @@ html = urllib2.urlopen(url).read()
 soup = BeautifulSoup(html)
 celebs = soup.find("div", {"class":"userNavigation"})
 
+f = open('celebs.txt','w')
+
 for anchor in celebs.findAll('a',href=True):
-    print anchor['href'].strip('/')
+    a = anchor['href'].strip('/')
+    f.write(a)
+    f.write('\n')
 
-
+f.close()
